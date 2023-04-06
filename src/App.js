472067896308fc829f8ee5a5b9edc7a1f4173e49
JMinio <juli.mno18@gmail.com>
2023-04-06 20:36:05 -0300

    estructura de tasklist creada - fin de primera parte

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TaskCreator } from './Components/TaskCreator';
+
+function App() {
+
+  return (
+    <div className="App">
+      <TaskCreator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TaskCreator.js
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+
+export const TaskCreator = () => {
+
+    const [newTasking, setNewTasking] = useState()
+
+    const handleSubmit = (e) => { /* funcion para capturar los datos */
+        e.preventDefault();
+        localStorage.setItem('task', newTasking) //localstorage para guardar "clave"=task y lo que el usuario escribe
+        setNewTasking(''); //sirve para limpiar el input una vez guardado.  
+    }
+
+    return (
+
+        <form onSubmit={handleSubmit}> {/* Usar para capturar los datos antes de que se envien */}
+            <input
+                type="text"
+                placeholder="Enter a new task"
+                value={newTasking}
+                onChange={(e) => setNewTasking(e.target.value)} /> {/* guardamos el valor que el user ingreso en el input */}
+
+            <button>Save Task</button> {/* Queremos ver que valor ingreso en el input */}
+        </form>
+    )
+}
